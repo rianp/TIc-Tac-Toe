ffeb8796623fd80700d0654bb38c914b14e3ab0d
Ryan Pickell <rian@Ryans-MBP.attlocal.net>
2023-03-09 18:13:33 -0800

    changed test to print string mock test, changed print method to print string, made console class its own module

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 """
-The Tic-Tac-Toe game console interface.
-
-This module defines a `Console` class representing a console interface for the
-Tic-Tac-Toe game. It also includes a `main` function that creates a `Console`
-object and calls its `greeting` method to display a welcome message to the user.
+This module contains the `main` function that creates any
+necessary objects and calls functions to execute the program's logic.
 """
 
-class Console:
-    """ A class representing a console interface for Tic-Tac-Toe game. """
-    def greeting(self):
-        """  Returns a welcome message to the user. """
-        return "Welcome to Tic-Tac-Toe."
+
+from console import *
 
 
 def main():
     """ Creates any necessary objects and calls functions to execute the program's logic. """
-    welcome = Console()
-    print(welcome.greeting())
+    Console.print_string("Hello friend, welcome to Tic-Tac-Toe!")
+
+    message = """
+*------------------------------------------------------------*  
+* Here are the instructions to the game!                     *
+*------------------------------------------------------------*
+* 1. there are two players in the game (X and O)             *
+* 2. a game has nine fields in a 3x3 grid                    *
+* 3. a player can take a field if not already taken          *
+* 4. players take turns taking fields until the game is over *
+* 5. a game is over when:                                    *
+*   - all fields in a row are taken by a player              *
+*   - all fields in a column are taken by a player           *
+*   - all fields in a diagonal are taken by a player         *
+*   - all fields are taken                                   *
+*------------------------------------------------------------* 
+"""
+
+    Console.print_string(message)
 
 
 if __name__ == "__main__":
